9ecdb81ddc44c9872f9141a444f0727e1c3fd0d2
Youssef <YoussefAhab@outlook.com>
2023-02-01 02:43:32 +0200

    Project skeleton

---
+++ zero2prod/target/debug/deps/zero2prod-2926a5cfd5e64b06.d
@@ -0,0 +1,5 @@
+/Users/youssefahab/Development/WavTrim/zero2prod/target/debug/deps/zero2prod-2926a5cfd5e64b06.rmeta: src/main.rs
+
+/Users/youssefahab/Development/WavTrim/zero2prod/target/debug/deps/zero2prod-2926a5cfd5e64b06.d: src/main.rs
+
+src/main.rs:
---
+++ zero2prod/target/debug/deps/zero2prod-47ab95e5774d5185.d
@@ -0,0 +1,5 @@
+/Users/youssefahab/Development/WavTrim/zero2prod/target/debug/deps/zero2prod-47ab95e5774d5185.rmeta: src/main.rs
+
+/Users/youssefahab/Development/WavTrim/zero2prod/target/debug/deps/zero2prod-47ab95e5774d5185.d: src/main.rs
+
+src/main.rs:
